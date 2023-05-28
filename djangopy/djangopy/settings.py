cbd3f21821474aeb2218da451efdb5cbc13e5bad
masoodsheikh <masooodsheikh73@gmail.com>
2023-05-29 00:31:56 +0500

    local databse connected

--- djangopy/djangopy/settings.py
+++ djangopy/djangopy/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'djangopy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myjobportal',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',  # Typically 'localhost' or '127.0.0.1'
+        'PORT': '3306',  # Typically '3306' for MySQL
     }
 }
 
