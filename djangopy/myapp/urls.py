53da33503dd20b656bd95fb026645ac7b2b70c1c
masoodsheikh <masooodsheikh73@gmail.com>
2023-06-01 01:55:17 +0500

    login, logout incorporated

--- djangopy/myapp/urls.py
+++ djangopy/myapp/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     
-    path('', views.home, name=""), 
+    path('', views.home, name="home"), 
+    path('login/', views.login_user, name="login"),
+    path('logout/', views.logout_user, name="logout"),
+    path('register/', views.register, name="register"),
 
 ]
--- djangopy/myapp/views.py
+++ djangopy/myapp/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     return render(request, 'myapp/index.html')
     
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in")
+            return redirect('home')
+        else:
+            messages.success(request, "Something went wrong, try again!")
+            return redirect('login')
+    else:
+        return render(request, "myapp/login.html")
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You are successfully logged out!")
+    return redirect('home')
+
+def register(request):
+    pass
